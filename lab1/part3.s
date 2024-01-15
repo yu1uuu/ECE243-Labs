b33b82e11e690f65089b650ab74e78def321fc57
Skye Nam <123508972+skyenam@users.noreply.github.com>
2024-01-15 13:20:53 -0500

    Update part3.s

--- lab1/part3.s
+++ lab1/part3.s
@@ -1,16 +1,19 @@
 .global _start
 _start:
-		
+		.equ LEDs, 0xFF200000
 		movi r8, 1
 		movi r9, 30
 		movi r12, 0
+  		
 		
 myloop: 
 
 		addi r8, r8, 1
 		add r12, r12, r8
 		ble r8, r9, myloop
-		
+
+  		movia r25, LEDs
+    		stwio r12, (r25)
 fin: br fin
 	
 	
