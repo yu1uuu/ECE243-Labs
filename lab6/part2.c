9db3e8286f97a6501252942bca5c4fa67e341aa9
Yulu Jiang <89326638+yu1uuu@users.noreply.github.com>
2024-02-25 16:42:51 -0500

    Update part2.c

--- lab6/part2.c
+++ lab6/part2.c
@@ -1,35 +1,26 @@
-/* Here is a new version of the code given on Wednesday, using structs */
+
 #define AUDIO_BASE 0xFF203040
 int main(void) {
-	// Audio port structure
-	struct audio_t {
-	volatile unsigned int control; // The control/status register
-	volatile unsigned char rarc; // the 8 bit RARC register
-	volatile unsigned char ralc; // the 8 bit RALC register
-	volatile unsigned char wsrc; // the 8 bit WSRC register
-	volatile unsigned char wslc; // the 8 bit WSLC register
-	volatile unsigned int ldata;
-	volatile unsigned int rdata;
-	};
-
-	/* we don't need to 'reserve memory' for this, it is already there
-	so we just need a pointer to this structure */
-	struct audio_t *const audiop = ((struct audio_t *) AUDIO_BASE);
-
-	// to hold values of samples
-	int left, right;
-
-	// infinite loop checking the RARC to see if there is at least a single
+	// Audio codec Register address
+	volatile int * audio_ptr = (int *) AUDIO_BASE;
+	
+	// intermediate values
+	int left, right, fifospace;
+	
+	// This is an infinite loop checking the RARC to see if there is at least a single
 	// entry in the input fifos. If there is, just copy it over to the output fifo.
 	// The timing of the input fifo controls the timing of the output
-
+	
 	while (1) {
-		if ( audiop->rarc > 0){ // check RARC to see if there is data to read
+		fifospace = *(audio_ptr + 1); // read the audio port fifospace register
+		if ((fifospace & 0x000000FF) > 0) // check RARC to see if there is data toread
+		{
 			// load both input microphone channels - just get one sample from each
-			left = audiop->ldata; // load the left input fifo
-			right = audiop->rdata; // load the right input fifo
-			audiop->ldata = left; // store to the left output fifo
-			audiop->rdata = right; // store to the right output fifo
+			int left = *(audio_ptr + 2);
+			int right = *(audio_ptr + 3);
+			// store both of those samples to output channels
+			*(audio_ptr + 2) = left;
+			*(audio_ptr + 3) = right;
 		}
 	}
 }
