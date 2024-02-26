be2aefa5a63dc1cc8726441312ebdf0abe98d12b
Skye Nam <123508972+skyenam@users.noreply.github.com>
2024-02-25 22:26:37 -0500

    lab6

--- lab6/part1.c
+++ lab6/part1.c
@@ -1,26 +1,19 @@
-int main(void){
-	volatile int*LEDR_ptr = 0xff200000;
-	volatile int*KEY_ptr = 0xff200050;
-	int LED_state = 0;
-	
-	// clear everything just in case
-	*LEDR_ptr = 0;
-	 *(KEY_ptr + 3) = 0;
-	
-	
-	while(1){
-		if(!LED_state){
-			if(*(KEY_ptr + 3) & 0x1){
-				*(KEY_ptr + 3) = 0xffffffff;
-				*(LEDR_ptr) = 0xffffffff;
-				LED_state = 1;
-			}
-		}else{
-			if( *(KEY_ptr + 3) & 0x2){
-				*(KEY_ptr + 3) = 0xffffffff;
-				*(LEDR_ptr) = 0;
-				LED_state = 0;
-			}
-		}
-	}
+int main(){
+    volatile int *LEDs = (int*) 0xFF200000;
+    volatile int *KEYs = (int*) 0xFF200050;
+    int edge_cap, KEY0, KEY1;
+    while(1){
+        edge_cap = *(KEYs + 3);
+        KEY0 = edge_cap & 0b1;
+        KEY1 = edge_cap & 0b10;
+        if (KEY0){
+            *LEDs = 0b1111111111;
+            *(KEYs + 3) = 0b1111; //reset edge counter
+        } else if (KEY1) {
+            *LEDs = 0;
+            *(KEYs + 3) = 0b1111; //reset edge counter
+        }
+        
+    }
+
 }
--- lab6/part3.c
+++ lab6/part3.c
@@ -1,45 +1,23 @@
-#define AUDIO_BASE 0xFF203040
-int main(void) {
-	// Audio port structure
-	struct audio_t {
-	volatile unsigned int control; // The control/status register
-	volatile unsigned char rarc; // the 8 bit RARC register
-	volatile unsigned char ralc; // the 8 bit RALC register
-	volatile unsigned char wsrc; // the 8 bit WSRC register
-	volatile unsigned char wslc; // the 8 bit WSLC register
-	volatile unsigned int ldata;
-	volatile unsigned int rdata;
-	};
+int main(){
+    volatile int * audio_ptr = (int*) 0xFF203040;
+    volatile int * SW_ptr = (int*) 0xFF200040;
+    
+    int freq, SW, num_burst;
+    while (1){
 
-	struct audio_t *const audiop = ((struct audio_t *) AUDIO_BASE);
-	volatile int *swp = 0xff200040;
-
-    // unsigned int period_samples = 8000 / frequency; // Total samples per period
-	int frequency;
-    int high = 16777215; 
-    int low = 0; 
-    int value = 0; 
-	int halfWaveSamples;
-	int sampleCount = 0;
-
-    while (1) {
-		frequency = 100 + (*swp % 10) * 190; // maps 0-9 switch value to 100Hz to 2000Hz
-		halfWaveSamples = 8000/(2*frequency); // recalculate for the current frequency
-		
-        // Toggle the value every half wave cycle
-        if (sampleCount >= halfWaveSamples) {
-            value = (value == high) ? low : high;
-      
-			
-			sampleCount = 0; // Reset the counter after one complete cycle
+        //code to figure out the freq based off val of switches
+        SW = *SW_ptr & 0b1111111111; //lower ten 1s
+        freq = 100 + (1900 * SW / 1024);
+        num_burst = 8000 / (2 * freq);
+        //code to output wave
+        if ((*(audio_ptr + 1) & 0x00FF0000) > 0) {    
+            for (int i = 0; i < 2; i++){ //make square wave by alternating between outputting a const val and 0
+                for (int j = 0; j < num_burst; j++){
+                    *(audio_ptr + 2) = i * 0x00FFFFFF;
+                    *(audio_ptr + 3) = i * 0x00FFFFFF;
+                }
+            }
         }
 
-        // Check if there's space in the FIFO before writing
-        if (audiop->wsrc > 0) {
-            audiop->ldata = value; // Write to left channel
-            audiop->rdata = value; // Write to right channel
-        }
-		sampleCount++;
     }
 }
-
